5fb4764abdc711d4bd1262b4aa3426978eac6606
cristophersaldanay <cristopher.saldanay@gmail.com>
2023-07-29 03:22:50 -0400

    init project

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application } from 'express'
+import routerHealth from './helpers/health'
+import HandlerErrors from './helpers/errors'
+
+class App {
+	readonly expressApp: Application
+
+	constructor() {
+		this.expressApp = express()
+		this.mountHealthCheck()
+		this.mountMiddlewares()
+
+		this.mountError()
+	}
+
+	mountHealthCheck() {
+		this.expressApp.use('/', routerHealth)
+	}
+
+	mountMiddlewares() {
+		this.expressApp.use(express.json())
+
+		this.expressApp.use(express.urlencoded({ extended: true }))
+	}
+
+	mountError(): void {
+		this.expressApp.use(HandlerErrors.notFound)
+	}
+}
+
+export default new App().expressApp
