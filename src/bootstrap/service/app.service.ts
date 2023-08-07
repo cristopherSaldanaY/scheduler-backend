47705b0af239b54954e846380b75aee551dfa35d
cristophersaldanay <cristopher.saldanay@gmail.com>
2023-08-07 10:23:09 -0400

    test

--- src/bootstrap/service/app.service.ts
+++ src/bootstrap/service/app.service.ts
@@ -7,12 +7,12 @@ export class AppService {
 
 	static get DBConfig(): DB_CONFIG {
 		return {
-			host: process.env.DB_HOST || 'localhost',
+			host: process.env.DB_HOST || '35.232.45.61',
 			port: +process.env.DB_PORT || 5432,
 			entities: [process.env.DB_ENTITIES || 'dist/**/*.entity.js'],
-			username: process.env.DB_USER || 'adminUser',
-			password: process.env.DB_PASS || '12345',
-			database: process.env.DB_NAME || 'bddnode',
+			username: process.env.DB_USER || 'postgres',
+			password: process.env.DB_PASS || 'routing.0811',
+			database: process.env.DB_NAME || 'bddtest',
 			synchronize: true,
 			logging: process.env.DB_LOGG == 'true' ? true : false,
 		}
